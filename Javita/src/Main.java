6b857dcc06d8338df8d4f4749f724305b0a332f7
Tomás <tomas.gonzalez@udla.edu.ec>
2025-10-01 08:01:30 -0500

    Update Main.java

--- Javita/src/Main.java
+++ Javita/src/Main.java
@@ -1,7 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.module.InvalidModuleDescriptorException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //Creacion de objetos
         Automovil auto1=new Automovil();  //Constructor:Separa memoria para el objeto creado, //Destructor: Elimina la memoria que no se usa
         Automovil auto2=new Automovil("BMV", "i320","",3.2 );
@@ -11,25 +15,34 @@ public class Main {
         auto1.color= "Negro";
         auto1.cilindraje= 3.2;
 
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Ingrese el nombre de la marca: ");
+        auto1.marca= br.readLine();
+        System.out.println("El nombre del modelo es :" + auto1.modelo);
+        System.out.println("");
+
+        auto1.marca= JOptionPane.showInputDialog("Ingrese el nombre de la marca:");
+        JOptionPane.showMessageDialog(null,"El modelo del vehiculo es:" +auto1.modelo);
+
+
         //Uso de metodos
         auto2.detalle();
 
         //Desplegar el valor de los objetos
-        System.out.println("Info del Main:");
+        /*System.out.println("Info del Main:");
         System.out.println("auto1.marca:" +auto1.marca);
         System.out.println("auto1.marca:"  +auto1.modelo);
         System.out.println("auto1.marca:" +auto1.color);
-        System.out.println("auto1.marca:" +auto1.cilindraje);
+        System.out.println("auto1.marca:" +auto1.cilindraje);*/
 
         //Creacion de objeto
-        Automovil formula1=new Automovil();
+        /*Automovil formula1=new Automovil();
 
         // Asignar valores a atributos
 
         formula1.marca= "Mercedes";
         formula1.modelo= "J26";
         formula1.color= "Blanco";
-        formula1.cilindraje= 4.1;
+        formula1.cilindraje= 4.1;*/
     }
 }
-
