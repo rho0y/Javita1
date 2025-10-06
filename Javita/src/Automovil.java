75e786adaa4bb43461c7a209d9ef7e2e97a2f4a0
Tomás <tomas.gonzalez@udla.edu.ec>
2025-10-06 10:20:55 -0500

    Refactor Automovil class with new property and method
    Updated the Automovil class to include a new property 'inyeccion' and modified the detalle method to use JOptionPane for displaying information.

--- Javita/src/Automovil.java
+++ Javita/src/Automovil.java
@@ -1,9 +1,13 @@
+import javax.swing.*;
+
 public class Automovil {
+    /**Declaracion de las propiedades*/
     String marca;
     String modelo;
     String color = "Blanco";
     double cilindraje;
-
+    double inyeccion;
+/**Declaracion de lso constructores y destructores*/
     public Automovil() {
     }
 
@@ -20,12 +24,22 @@ public class Automovil {
     }
 
     //System.out.println("Info del metodo")
-    public void detalle(){
-        String modelo="AUDI";
+    /**Uso combinado de scan y showMessage*/
+    public String detalle(String marca){
+        String c;
+       /* String modelo="AUDI";
         System.out.println("marca:" +this.marca);
         System.out.println("modelo:"  +this.modelo);
         System.out.println("color:" +this.color);
         System.out.println("cilindraje:" +this.cilindraje);
+        JOptionPane.showMessageDialog(null,"Marca: "+this.marca);*/
+        /*JOptionPane.showMessageDialog(null, "Marca\n" +marca);
+        JOptionPane.showMessageDialog(null, "Modelo\n" +modelo);
+        JOptionPane.showMessageDialog(null, "Color\n" +color);
+        JOptionPane.showMessageDialog(null, "Cilindraje\n" +cilindraje);*/
+        JOptionPane.showMessageDialog(null, "Marca\n " +marca);
+        c=marca +"Evolution";
+        return c;
     }
 }
 
